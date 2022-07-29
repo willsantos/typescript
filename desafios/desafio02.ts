1375cdd98f9a95159aa04fd8f2b7ed94e946f299
Wilson Santos <contato@wilsonsantos.net>
2022-07-29 19:01:45 -0300

    ✨ desafio02 solved

--- desafios/desafio02.ts
+++ desafios/desafio02.ts
@@ -1,23 +1,39 @@
-// Como podemos melhorar o esse código usando TS? 
+// Como podemos melhorar o esse código usando TS?
+//Criando uma interface para pessoa e um enum para as profissões
 
-let pessoa1 = {};
-pessoa1.nome = "maria";
-pessoa1.idade = 29;
-pessoa1.profissao = "atriz"
+interface Pessoa {
+  nome: string;
+  idade: number;
+  profissao: Profissoes;
+}
 
-let pessoa2 = {}
-pessoa2.nome = "roberto";
-pessoa2.idade = 19;
-pessoa2.profissao = "Padeiro";
+enum Profissoes {
+  Atriz,
+  Padeiro,
+}
 
-let pessoa3 = {
-    nome: "laura",
-    idade: "32",
-    profissao: "Atriz"
+let pessoa1: Pessoa = {
+  nome: "maria",
+  idade: 29,
+  profissao: Profissoes.Atriz,
 };
 
-let pessoa4 = {
-    nome = "carlos",
-    idade = 19,
-    profissao = "padeiro"
-}
+let pessoa2: Pessoa = {
+  nome: "roberto",
+  idade: 19,
+  profissao: Profissoes.Padeiro,
+};
+
+let pessoa3: Pessoa = {
+  nome: "laura",
+  idade: 32,
+  profissao: Profissoes.Atriz,
+};
+
+let pessoa4: Pessoa = {
+  nome: "carlos",
+  idade: 19,
+  profissao: Profissoes.Padeiro,
+};
+
+console.log(pessoa4);
