8557a126ef69ac7ecea07551ed480e45de5f4ee9
Wilson Santos <contato@wilsonsantos.net>
2022-07-29 18:50:00 -0300

    ✨ desafio01 solved

--- desafios/desafio01.ts
+++ desafios/desafio01.ts
@@ -1,5 +1,15 @@
-// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+// Como podemos rodar isso em um arquivo .ts sem causar erros?
+//Criando uma interface para o employee
 
-let employee = {};
+interface Funcionario {
+  code: number;
+  name: string;
+}
+
+const employee = {} as Funcionario;
+
 employee.code = 10;
 employee.name = "John";
+
+console.log(employee.code);
+console.log(employee.name);
